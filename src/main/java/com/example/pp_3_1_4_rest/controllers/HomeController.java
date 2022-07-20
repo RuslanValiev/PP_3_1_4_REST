8cc1e45551cf79affc1a71b985d04e362b8724fd
DomSSD <valievrusik@mail.ru>
2022-07-20 16:19:19 +0300

    Initial commit

---
+++ src/main/java/com/example/pp_3_1_4_rest/controllers/HomeController.java
@@ -0,0 +1,13 @@
+package com.example.pp_3_1_4_rest.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "redirect:/login";
+    }
+}
---
+++ src/main/java/com/example/pp_3_1_4_rest/controllers/MyController.java
@@ -0,0 +1,17 @@
+package com.example.pp_3_1_4_rest.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MyController {
+    @GetMapping("/admin")
+    public String admin() {
+        return "admin";
+    }
+
+    @GetMapping("/user")
+    public String user() {
+        return "user";
+    }
+}
